e6c5ade2842451047311ae4fdd78a58b4f2ae7bb
unknown <fncoder@gmail.com>
2018-06-07 17:14:04 +0200

    create component Main

---
+++ app/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Videos from 'components/Videos';
+
+const Main = () => (
+  <main className="main">
+    <div className="wrapper wrapper--main">
+      <Videos />
+    </div>
+  </main>
+);
+
+export default Main;
