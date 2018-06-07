bcc93075f3ea0752bd3b13edee321bbcb9e1c2bb
unknown <fncoder@gmail.com>
2018-06-07 15:47:43 +0200

    add text color title

--- app/components/Logo/Logo.jsx
+++ app/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = () => (
   <header className="logo">
-    <h1 className="logo__title">Search Videos</h1>
+    <h1 className="logo__title">Search <span className="text-red">Videos</span></h1>
     <p className="logo__text">Search amazing videos for free</p>
   </header>
 );
