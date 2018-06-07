8affafd1a26d9d2cb13203732b408e3a24f003b3
unknown <fncoder@gmail.com>
2018-06-07 13:31:19 +0200

    create component Logo

---
+++ app/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Logo = () => (
+  <header className="logo">
+    <h1 className="logo__title">Search Videos</h1>
+    <p className="logo__text">Search amazing videos for free</p>
+  </header>
+);
+
+export default Logo;
