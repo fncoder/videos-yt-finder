43033eec12636ae2a17e8bf6ae6031e248cadd5c
unknown <fncoder@gmail.com>
2018-06-07 19:21:47 +0200

    add export action

--- app/actions/addSearchValue.js
+++ app/actions/addSearchValue.js
@@ -1,10 +1,8 @@
 import { ADD_SEARCH_VALUE } from 'constants/actionTypes.js';
 
-const addSearchValue = value =>{
-  return{
-    type: ADD_SEARCH_VALUE,
-    payload: value
-  }
-}
+const addSearchValue = value => ({
+  type: ADD_SEARCH_VALUE,
+  payload: value,
+});
 
 export default addSearchValue;
---
+++ app/actions/index.js
@@ -0,0 +1,1 @@
+export { default as addSearchValue } from './addSearchValue.js';
