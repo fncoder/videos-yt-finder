e0e965bdbadab0a0a39fc46fb4392e7764a24445
unknown <fncoder@gmail.com>
2018-06-07 13:31:52 +0200

    create component SearchInput

---
+++ app/components/SearchInput/SearchInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchInput = () => (
+  <div className="search">
+    <input type="text" className="input-search" placeholder="Search Youtube" />
+  </div>
+);
+
+export default SearchInput;
