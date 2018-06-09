e4f4ca1e20c5ef9619056fdabccb5ed6c0e9e7f2
unknown <fncoder@gmail.com>
2018-06-09 18:37:32 +0200

    add support to keydown event

--- app/api/fetchVideos.js
+++ app/api/fetchVideos.js
@@ -1,7 +1,13 @@
 import { fetchVideosSuccess } from 'actions';
 
-const fetchVideos = () => (dispatch) => {
-  const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=google&type=video&maxResults=10&key=AIzaSyDwn1T4zBSOOn9ZCY1eo8gdOhO1ONsk4zA';
+const fetchVideos = searchValue => (dispatch) => {
+  let url;
+  if (searchValue) {
+    url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchValue}&type=video&maxResults=10&key=AIzaSyDwn1T4zBSOOn9ZCY1eo8gdOhO1ONsk4zA`;
+  } else {
+    url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=google&type=video&maxResults=10&key=AIzaSyDwn1T4zBSOOn9ZCY1eo8gdOhO1ONsk4zA';
+  }
+
   return fetch(url)
     .then(res => res.json())
     .then(res => dispatch(fetchVideosSuccess(res)))
