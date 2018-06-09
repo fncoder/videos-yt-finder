268dc746332ac8f8904f2483b6d7f451ab204c37
unknown <fncoder@gmail.com>
2018-06-09 15:50:01 +0200

    increase max results response

--- app/api/fetchVideos.js
+++ app/api/fetchVideos.js
@@ -1,7 +1,7 @@
 import { fetchVideosSuccess } from 'actions';
 
 const fetchVideos = () => (dispatch) => {
-  const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=google&type=video&maxResults=6&key=AIzaSyDwn1T4zBSOOn9ZCY1eo8gdOhO1ONsk4zA';
+  const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=google&type=video&maxResults=10&key=AIzaSyDwn1T4zBSOOn9ZCY1eo8gdOhO1ONsk4zA';
   return fetch(url)
     .then(res => res.json())
     .then(res => dispatch(fetchVideosSuccess(res)))
