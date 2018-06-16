c96922c30c2330e218fe3ecc328aa1536fa538d0
unknown <fncoder@gmail.com>
2018-06-16 20:39:03 +0200

    create action

---
+++ app/actions/fetchVideosRequest.js
@@ -0,0 +1,7 @@
+import { FETCH_VIDEOS_REQUEST } from 'constants/actionTypes.js';
+
+const fetchVideosRequest = value => ({
+  type: FETCH_VIDEOS_REQUEST,
+});
+
+export default fetchVideosRequest;
--- app/actions/index.js
+++ app/actions/index.js
@@ -3,3 +3,4 @@ export { default as fetchVideosSuccess } from './fetchVideosSuccess.js';
 export { default as fetchSearchValue } from './fetchSearchValue.js';
 export { default as showNavigate } from './showNavigate.js';
 export { default as fetchPageFilter } from './fetchPageFilter.js';
+export { default as fetchVideosRequest } from './fetchVideosRequest.js';
