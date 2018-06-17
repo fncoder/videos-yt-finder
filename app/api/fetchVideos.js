693a8227acfc3f708f4df8fb309b836993b52896
unknown <fncoder@gmail.com>
2018-06-17 15:32:50 +0200

    remove additional ampersand

--- app/api/fetchVideos.js
+++ app/api/fetchVideos.js
@@ -10,7 +10,7 @@ const fetchVideos = searchValue => (dispatch) => {
   if (searchValue) {
     url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchValue}&type=video&maxResults=10&key=AIzaSyDwn1T4zBSOOn9ZCY1eo8gdOhO1ONsk4zA`;
   } else {
-    url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=google&type=video&maxResults=10&&key=AIzaSyDwn1T4zBSOOn9ZCY1eo8gdOhO1ONsk4zA';
+    url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=google&type=video&maxResults=10&key=AIzaSyDwn1T4zBSOOn9ZCY1eo8gdOhO1ONsk4zA';
   }
 
   return fetch(url)
