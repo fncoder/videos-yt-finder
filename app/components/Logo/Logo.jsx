255ee6d8aa67010c24d2e664614271ac60769ca2
unknown <fncoder@gmail.com>
2018-06-18 12:40:59 +0200

    create animate subtitle

--- app/components/Logo/Logo.jsx
+++ app/components/Logo/Logo.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Logo = () => (
   <header className="logo">
     <h1 className="logo__title">Search <span className="text-red">Videos</span></h1>
-    <p className="logo__text">Search amazing videos for free</p>
+    <div className="animate-text">
+      <p className="logo__text">Search amazing videos for free</p>
+      <p className="logo__text">Search amazing videos for free</p>
+    </div>
   </header>
 );
 
