c7731919dba62fa7b1bcb75955741c8740ea2bd9
unknown <fncoder@gmail.com>
2018-06-20 19:59:18 +0200

    update reducer

--- app/reducers/fetchVideosData.js
+++ app/reducers/fetchVideosData.js
@@ -19,7 +19,7 @@ const fetchVideoData = (state, action) => {
     case FETCH_VIDEOS_SUCCESS:
       return Object.assign({ isFetch: true }, action.payload);
     case FETCH_VIDEOS_FAILURE:
-      return Object.assign({ isError: true }, action.payload);
+      return Object.assign({ isError: action.error }, action.payload);
     default:
       return state;
   }
